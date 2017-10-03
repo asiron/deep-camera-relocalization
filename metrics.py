d63a11a64e74074a004a1b15aae0db06987c6a00
Maciej Zurad <maciej.zurad@gmail.com>
2017-10-03 20:01:28 +0200

    fixing metrics

--- metrics.py
+++ metrics.py
@@ -29,75 +29,76 @@ class PoseMetrics(object):
   @staticmethod
   def rmse_position(y_true, y_pred):
     print type((y_true, y_pred))
-    errors = PoseMetrics.errors_position(y_true, y_pred)
+    errors = PoseMetrics.abs_errors_position(y_true, y_pred)
     return np.sqrt(np.mean(errors ** 2))
 
   @staticmethod
   def mae_position(y_true, y_pred):
-    errors = PoseMetrics.errors_position(y_true, y_pred)
+    errors = PoseMetrics.abs_errors_position(y_true, y_pred)
     return np.mean(np.abs(errors))
 
   @staticmethod
   def median_position(y_true, y_pred):
-    errors = PoseMetrics.errors_position(y_true, y_pred)
+    errors = PoseMetrics.abs_errors_position(y_true, y_pred)
     return np.median(errors)
 
   @staticmethod
   def stddev_position(y_true, y_pred):
-    errors = PoseMetrics.errors_position(y_true, y_pred)
+    errors = PoseMetrics.abs_errors_position(y_true, y_pred)
     return np.std(errors)
 
   @staticmethod
   def min_position(y_true, y_pred):
-    errors = PoseMetrics.errors_position(y_true, y_pred)
+    errors = PoseMetrics.abs_errors_position(y_true, y_pred)
     return np.min(errors)
 
   @staticmethod
   def max_position(y_true, y_pred):
-    errors = PoseMetrics.errors_position(y_true, y_pred)
+    errors = PoseMetrics.abs_errors_position(y_true, y_pred)
     return np.max(errors)
 
 
   @staticmethod
   def rmse_orientation(y_true, y_pred):
-    errors = PoseMetrics.errors_orienation(y_true, y_pred)
+    errors = PoseMetrics.abs_errors_orienation(y_true, y_pred)
     return np.sqrt(np.mean(errors ** 2))
 
   @staticmethod
   def mae_orientation(y_true, y_pred):
-    errors = PoseMetrics.errors_orienation(y_true, y_pred)
-    return np.mean(np.abs(errors))
+    errors = PoseMetrics.abs_errors_orienation(y_true, y_pred)
+    return np.mean(errors)
 
   @staticmethod
   def median_orientation(y_true, y_pred):
-    errors = PoseMetrics.errors_orienation(y_true, y_pred)
+    errors = PoseMetrics.abs_errors_orienation(y_true, y_pred)
     return np.median(errors)
 
   @staticmethod
   def stddev_orientation(y_true, y_pred):
-    errors = PoseMetrics.errors_orienation(y_true, y_pred)
+    errors = PoseMetrics.abs_errors_orienation(y_true, y_pred)
     return np.std(errors)
 
   @staticmethod
   def min_orientation(y_true, y_pred):
-    errors = PoseMetrics.errors_orienation(y_true, y_pred)
+    errors = PoseMetrics.abs_errors_orienation(y_true, y_pred)
     return np.min(errors)
 
   @staticmethod
   def max_orientation(y_true, y_pred):
-    errors = PoseMetrics.errors_orienation(y_true, y_pred)
+    errors = PoseMetrics.abs_errors_orienation(y_true, y_pred)
     return np.max(errors)
 
   @staticmethod
-  def errors_position(y_true, y_pred):
+  def abs_errors_position(y_true, y_pred):
     with timeit('position'):
       pos_true, pos_pred = y_true[..., :3], y_pred[..., :3]
       return np.linalg.norm(pos_true - pos_pred, axis=-1)
 
   @staticmethod
-  def errors_orienation(y_true, y_pred):
+  def abs_errors_orienation(y_true, y_pred):
     with timeit('orientation'):
       quat_true = quaternion.as_quat_array(y_true[..., 3:].copy())
       quat_pred = quaternion.as_quat_array(y_pred[..., 3:].copy())
       errors = (quat_true * quat_pred.conjugate())
-      return np.degrees([q.angle() for q in errors])
+      angles = np.degrees([q.angle() for q in errors])
+      return np.abs(np.where(angles > 180.0, angles - 360.0, angles))
